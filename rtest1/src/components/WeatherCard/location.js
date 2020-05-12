2a35058710269c6f41bc9abb62af70010f7b157c
Jeremiah Isaiah <jerboygt@gmail.com>
2020-05-13 00:32:43 +0700

    1st commit

---
+++ rtest1/src/components/WeatherCard/location.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Location = (props) => {
+  const Container = styled.div`
+    text-align: center;
+  `;
+
+  const City = styled.h1`
+    font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
+      sans-serif;
+    font-size: 1.6em;
+  `;
+
+  const Country = styled.h3`
+    font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
+      sans-serif;
+    font-size: 1.1rem;
+  `;
+
+  return (
+    <Container>
+      <City>Jakarta</City>
+      <Country>ID</Country>
+    </Container>
+  );
+};
+
+export default Location;
